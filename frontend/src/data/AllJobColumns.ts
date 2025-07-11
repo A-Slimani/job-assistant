fd497c08460b7f234cfcef18635b391e91d29c8d
Abdullah Slimani <abdullahslimani98@gmail.com>
2025-07-11 17:06:03 +1000

    first

--- frontend/src/data/AllJobColumns.ts
+++ frontend/src/data/AllJobColumns.ts
@@ -7,31 +7,31 @@ export const AllJobColumns: ColumnDef<Job>[] = [
     accessorKey: 'title',
     header: 'Job Title',
     cell: ({ row }) => {
-      // const rowValue = row.getValue('title')
-      // const length = typeof rowValue === 'string' ?  rowValue.length : 0
-      // const updatedValue = length > 50 ?
-      return h('div', { class: 'w-80' }, row.getValue('title'))
+      const rowValue: string = typeof row.getValue('title') === 'string' ? row.getValue('title') : ''
+      const length: number = rowValue.length
+      const updatedValue = length > 50 ? `${rowValue.substring(0, 48)} ...` : rowValue
+      return h('div', { class: 'w-80'}, updatedValue)
     }
   },
   {
     accessorKey: 'company',
     header: 'Company',
-    cell: ({ row }) => h('div', { class: 'w-60' }, row.getValue('company')),
+    cell: ({ row }) => h('div', { class: 'w-60' }, row.getValue('company'))
   },
   {
     accessorKey: 'location',
     header: 'Location',
-    cell: ({ row }) => h('div', row.getValue('location')),
+    cell: ({ row }) => h('div', row.getValue('location'))
   },
   {
     accessorKey: 'website',
     header: 'Website',
-    cell: ({ row }) => h('div',  row.getValue('website')),
+    cell: ({ row }) => h('div', row.getValue('website'))
   },
   {
     accessorKey: 'createdAt',
     header: 'Created At',
-    cell:  ({ row }) => h('div', row.getValue('createdAt')),
+    cell: ({ row }) => h('div', row.getValue('createdAt'))
   },
   {
     header: 'Post Date',
@@ -40,6 +40,6 @@ export const AllJobColumns: ColumnDef<Job>[] = [
   {
     accessorKey: 'status',
     header: 'Status (to change to a dropdown)',
-    cell: ({ row }) => h('div', row.getValue('status')),
-  },
+    cell: ({ row }) => h('div', row.getValue('status'))
+  }
 ]
